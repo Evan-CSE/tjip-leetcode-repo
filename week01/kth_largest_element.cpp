5d428c67c86d495a015ec6ff585511fa60bbf16f
Evan-CSE <evanshareef@gmail.com>
2022-12-02 23:13:18 +0600

    Kth Largest Element Added

---
+++ week01/kth_largest_element.cpp
@@ -0,0 +1,22 @@
+// Approach 1
+// Complexity: o(n)
+class Solution
+{
+public:
+    int findKthLargest(vector<int> &nums, int k)
+    {
+        int ValueCount[100040] = {0};
+        for (auto x : nums)
+            ValueCount[x + 10000]++;
+        int cnt = 0;
+        for (int i = 20000; i >= 0; i--)
+        {
+            cnt += ValueCount[i];
+            if (cnt >= k)
+            {
+                return i - 10000;
+            }
+        }
+        return 10000;
+    }
+};
