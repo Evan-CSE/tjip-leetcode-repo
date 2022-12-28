3e938e289b3bab29f4df83624f0df655b6bc4f92
Evan-CSE <evanshareef@gmail.com>
2022-12-28 12:02:51 +0600

    Remove nth node updated with fast and slow pointer concept

--- week03/remove_nth_node_from_end_of_the_list.cpp
+++ week03/remove_nth_node_from_end_of_the_list.cpp
@@ -6,29 +6,22 @@
 class Solution
 {
 public:
-    int determineSize(ListNode *head)
-    {
-        int size = 0;
-        ListNode *current_node = new ListNode(INT_MAX, NULL);
-        current_node->next = head;
-        current_node = current_node->next;
-        while (current_node)
-            current_node = current_node->next, size++;
-        return size;
-    }
     ListNode *removeNthFromEnd(ListNode *head, int n)
     {
-        int size_of_list = determineSize(head);
-        n = size_of_list - n;
-        ListNode *current_node_head = new ListNode(INT_MAX, NULL);
-        current_node_head->next = head;
-        ListNode *node = current_node_head;
-        while (n--)
-            node = node->next;
-        ListNode *del_node;
-        del_node = node->next;
-        node->next = node->next->next;
-        delete (del_node);
-        return current_node_head->next;
+        auto fast = head;
+        ListNode *dummy_node = new ListNode(0, head);
+        auto slow = dummy_node;
+        int displacement = 0;
+        while (displacement < n)
+            fast = fast->next, displacement++;
+        while (fast)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        fast = slow->next;
+        slow->next = slow->next->next;
+        delete (fast);
+        return dummy_node->next;
     }
 };
