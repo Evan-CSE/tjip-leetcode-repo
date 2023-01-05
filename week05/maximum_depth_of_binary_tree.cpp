c69af5987f300bf68c98cf012c6027c81c122a07
Evan-CSE <evanshareef@gmail.com>
2023-01-06 00:47:57 +0600

    Maximum Depth of binary tree solved

---
+++ week05/maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,16 @@
+/**
+ * TC: O(n)
+ * MC: O(n)
+ */
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+        auto left_depth = maxDepth(root->left);
+        auto right_depth = maxDepth(root->right);
+        return 1 + max(left_depth, right_depth);
+    }
+};
