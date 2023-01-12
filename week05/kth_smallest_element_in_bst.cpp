bbf9e8005c4f52d6a05afd65ee6b25368afe55d8
Evan-CSE <evanshareef@gmail.com>
2023-01-12 20:16:43 +0600

    Kth smallest element in bst

---
+++ week05/kth_smallest_element_in_bst.cpp
@@ -0,0 +1,32 @@
+/**
+ *  TC: O(n)
+ *  MC: O(h)
+ * where n is the number of nodes of the tree and h is the height of the bst
+ */
+
+class Solution
+{
+public:
+    int value = INT_MAX;
+    int idx;
+    void traverse_and_find(TreeNode *node)
+    {
+        if (node == NULL || idx < 0)
+            return;
+        traverse_and_find(node->left);
+        idx--;
+        if (idx == 0)
+        {
+            value = min(node->val, value);
+            idx--;
+            return;
+        }
+        traverse_and_find(node->right);
+    }
+    int kthSmallest(TreeNode *root, int k)
+    {
+        idx = k;
+        traverse_and_find(root);
+        return value;
+    }
+};
