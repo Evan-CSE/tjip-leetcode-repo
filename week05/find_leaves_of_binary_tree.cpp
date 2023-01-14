cb50e272ffd769d00f272b29e47382525319bde6
Evan-CSE <evanshareef@gmail.com>
2023-01-14 22:36:13 +0600

    Find leaves of binary tree solution optimised

--- week05/find_leaves_of_binary_tree.cpp
+++ week05/find_leaves_of_binary_tree.cpp
@@ -1,48 +1,28 @@
 /**
- *  TC: O(nlog(n))
+ *  TC: O(n)
  *  MC: O(n)
- *  where 'n' is the number of nodes of the tree
+ * where 'n' is the number of nodes of this tree
  */
-
 class Solution
 {
 public:
-    map<TreeNode *, int> max_depth; // we don't know if all values are unique so we should map their max_depth with corresponding TreeNode
-    int max_number_of_vectors = 0;
     // a node 'n' will be unotouched until max depth from n to leaf
-    int find_max_depth(TreeNode *root)
+    int find_max_depth(TreeNode *root, vector<vector<int>> &leaves)
     {
         if (root == NULL)
         {
-            max_depth[root] = -1;
             return 0;
         }
-        if (max_depth.count(root))
-            return max_depth[root];
-        int left_max_depth = find_max_depth(root->left);
-        int right_max_dept = find_max_depth(root->right);
-        if (root->left)
-            max_depth[root->left] = left_max_depth;
-        if (root->right)
-            max_depth[root->right] = right_max_dept;
-        max_depth[root] = max(left_max_depth, right_max_dept) + 1;
-        max_number_of_vectors = max(max_number_of_vectors, max_depth[root]);
-        return max_depth[root];
+        int depth = max(find_max_depth(root->left, leaves), find_max_depth(root->right, leaves)) + 1;
+        if (depth == leaves.size() + 1)
+            leaves.push_back({});
+        leaves[depth - 1].push_back(root->val);
+        return depth;
     }
     vector<vector<int>> findLeaves(TreeNode *root)
     {
         vector<vector<int>> leaves;
-        find_max_depth(root);
-        map<int, vector<int>> mp;
-        for (auto it = max_depth.begin(); it != max_depth.end(); it++)
-        {
-            if (it->second >= 0)
-                mp[it->second].push_back(it->first->val);
-        }
-        for (auto it = mp.begin(); it != mp.end(); it++)
-        {
-            leaves.push_back(it->second);
-        }
+        find_max_depth(root, leaves);
         return leaves;
     }
 };
